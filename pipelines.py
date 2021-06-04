a04973fe72253177c2497435d62956fc39d40475
jpekmez <jpekmez@gmail.com>
2021-06-04 20:55:24 +0200

    Update pipelines.py
    fix answer_text

--- pipelines.py
+++ pipelines.py
@@ -139,6 +139,7 @@ class QGPipeline:
                 
                 answer_text = answer_text.strip()
                 
+                answer_text = re.sub("<pad> | <pad>", "", answer_text)
                 ans_start_idx = sent.index(answer_text)
                 
                 sent = f"{sent[:ans_start_idx]} <hl> {answer_text} <hl> {sent[ans_start_idx + len(answer_text): ]}"
